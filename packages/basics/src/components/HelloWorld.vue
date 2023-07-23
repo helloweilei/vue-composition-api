382c34abfb75bfcc4a28dd7c6ca07992d8bfa660
Wei Lei <lei.wei@thoughtworks.com>
2023-07-23 17:11:36 +0800

    add new code

--- packages/basics/src/components/HelloWorld.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-
-</script>
-
-<template>
-  <div class="hello">Hello</div>
-</template>
---
+++ packages/basics/src/components/MyCounter.vue
@@ -0,0 +1,28 @@
+<template>
+  <h3>{{ counterData.title }}</h3>
+  <div class="counter">
+    <button @click="increase">+</button>
+    <span>{{ counter }}</span>
+    <button @click="decrease">-</button>
+  </div>
+  <input type="text" v-model="counterData.title" />
+</template>
+
+<script setup lang="ts">
+import { reactive, ref } from 'vue';
+
+const counter = ref(0);
+const counterData = reactive({
+  title: 'My Counter'
+});
+
+const increase = () => counter.value++;
+const decrease = () => counter.value--;
+</script>
+
+<style scoped>
+.counter {
+  display: flex;
+  align-items: center;
+}
+</style>
