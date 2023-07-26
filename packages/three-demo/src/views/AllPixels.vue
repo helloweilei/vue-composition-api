2485bb30f002790f6e221e685ab00fb89257f3df
Wei Lei <lei.wei@thoughtworks.com>
2023-07-26 18:03:00 +0800

    add more shape

--- packages/three-demo/src/views/AllPixels.vue
+++ packages/three-demo/src/views/AllPixels.vue
@@ -7,40 +7,62 @@ import * as THREE from 'three'
 import { onMounted, ref } from 'vue'
 import initCanvas from '../utils/initCanvas'
 import { createDirectionalLight } from '../utils/light'
+import { createMaterial } from '../utils/material'
 
 const canvas = ref<HTMLCanvasElement | undefined>()
-const material = new THREE.MeshPhongMaterial({ color: 0x44aa88 })
-const material2 = new THREE.MeshPhongMaterial({ color: 0xff8822 })
-const geometries: Array<any> = []
+const objects: Array<THREE.Object3D> = []
 
 const circleGeometry = new THREE.CircleGeometry(0.4, 100)
-const circle = new THREE.Mesh(circleGeometry, material)
+const circle = new THREE.Mesh(circleGeometry, createMaterial())
 
 const coneGeometry = new THREE.ConeGeometry(
   0.4, 1,
   40, 40,
   false,
   0, Math.PI * 2);
-const cone = new THREE.Mesh(coneGeometry, material)
+const cone = new THREE.Mesh(coneGeometry, createMaterial())
 
 const cylinderGeometry = new THREE.CylinderGeometry(
   0.4, 0.4, 1, 20);
-const cylinder = new THREE.Mesh(cylinderGeometry, material2)
+const cylinder = new THREE.Mesh(cylinderGeometry, createMaterial())
 cylinder.position.x = -1
 
+const dodecahedronGeometry = new THREE.DodecahedronGeometry(0.4, 4)
+const dodecahedron = new THREE.Mesh(dodecahedronGeometry, createMaterial())
+dodecahedron.position.x = 1
+
+const sphereGeometry = new THREE.SphereGeometry(0.6, 10, 10)
+const sphere = new THREE.Mesh(sphereGeometry, createMaterial())
+sphere.position.y = 1.3
+
+const wireFrameGeometry = new THREE.WireframeGeometry(
+  new THREE.BoxGeometry(1, 1, 1, 1, 1, 1)
+)
+const wireFrame = new THREE.Mesh(wireFrameGeometry, createMaterial())
+wireFrame.position.y = -1.3
+
+const material = new THREE.PointsMaterial({
+  color: 'red',
+  size: 0.1,     // in world units
+})
+const points = new THREE.Points(sphereGeometry, material)
+points.position.x = 2
+
 const light = createDirectionalLight([-1, 2, 4])
-geometries.push(circle, cone, cylinder)
+objects.push(circle, cone, cylinder, dodecahedron, sphere, wireFrame,
+  points
+)
 
 onMounted(() => {
   const { scene, camera, renderer } = initCanvas(canvas.value!)
 
-  geometries.forEach(geo => scene.add(geo))
+  objects.forEach(geo => scene.add(geo))
   scene.add(light)
   camera.position.z = 5
 
   function animate() {
     requestAnimationFrame(animate)
-    geometries.forEach(geometry => {
+    objects.forEach(geometry => {
       geometry.rotation.x += 0.01
       geometry.rotation.y += 0.01
     })
